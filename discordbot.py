afd824d7757a9a62a9477cee3f3de65d9aa44222
gangmin08 <105585147+gangmin08@users.noreply.github.com>
2023-02-16 01:13:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,34 +1,75 @@
-from cmath import log
-from distutils.sysconfig import PREFIX
+from discord.ext import commands
 import discord
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
-PREFIX = os.environ['PREFIX']
-TOKEN = os.environ['TOKEN']
+BOT_TOKEN = "MTA3NDkwMDE2NzI2Nzk5MTY0Mg.GFVzZZ.2JFZ3l0bd6gsVLjnLK8YWdjuWziGO7gM_YeV10"
+CHANNEL_ID = 1074259672304984126
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
+    print("Hello! Katze bot is ready!")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("Hello! Katze bot is ready!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def whatareyoudoing(ctx):
+    await ctx.send("Nothing")
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
+@bot.command()
+async def Hello(ctx):
+    await ctx.send("Hello!")
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
-        
-    if message.content == f'{PREFIX}hi':
-        await message.channel.send('Hi!')
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
-try:
-    client.run(TOKEN)
-except discord.errors.LoginFailure as e:
-    print("Improper token has been passed.")
+@bot.command()
+async def Hi(ctx):
+    await ctx.send("Hi!")
+
+@bot.command()
+async def Bye(ctx):
+    await ctx.send("Good Bye!")
+
+@bot.command()
+async def add(ctx, x, y):
+    result = int(x) + int(y)
+    await ctx.send(f"{x} + {y} = {result}")
+
+@bot.command()
+async def sub(ctx, x, y):
+    result = int(x) - int(y)
+    await ctx.send(f"{x} - {y} = {result}")
+
+@bot.command()
+async def mult(ctx, x, y):
+    result = int(x) * int(y)
+    await ctx.send(f"{x} * {y} = {result}")
+
+@bot.command()
+async def div(ctx, x, y):
+    result = int(y) / int(x)
+    await ctx.send(f"{y} / {x} = {result}")
+
+@bot.command()
+async def hahaha(ctx):
+    await ctx.send("Amazing!")
+
+@bot.command()
+async def whatcanyoudo(ctx):
+    await ctx.send("I can do anything soon")
+
+@bot.command()
+async def good(ctx):
+    await ctx.send("Thank you!")
+
+@bot.command()
+async def Help(ctx):
+    await ctx.send("Nope")
+
+@bot.command()
+async def gangmin(ctx):
+    await ctx.send("Handsome Man")
+
+bot.run(BOT_TOKEN)
